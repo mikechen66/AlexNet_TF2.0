2fd99b8c5f3cc8fa4b1072331dc54336d0cee88a
mikechen66 <875661606@qq.com>
2020-07-06 05:40:34 +0800

    Update client_one.py
    Streamline the comments

--- client_one.py
+++ client_one.py
@@ -1,39 +1,7 @@
 
 """
-Author: Henry Powell
-Institution: Institute of Neuroscience, Glasgow University, Scotland.
-Implementation of AlexNet using Keras with Tensorflow backend. Code will preload the Oxford_Flowers102 dataset.
-Learning tracks the model's accuracy, loss, and top 5 error rate. For true comparision of performance to the original
-model (Krizhevsky et al. 2010) this implementation will need to be trained on Imagenet2010.
-
-Editor: Mike Chen
-Split the client appliction from the AlexNet model for better readibility, accessibiliuty and usability. Developers 
-need to install the following libraries. 
-
-1. Normally speaking, we only nned to install tensorflow_datasets
-
-$ pip install tensorflow_datasets
-or 
-$ pip3 install tensorflow_datasets
-
-While running the script of main.py, it will build a directory "/home/user/tensorflow_datasets". It has two sub-directories 
-including downloads, oxford_flowers102. I assume that developers uses Ubuntu 18.04. 
-
-2. Install tfds-nightly
-
-If it raises NonMatchingChecksumError(resource.url, tmp_path) duruing the runtime, please make the following changes to adapt 
-to Oxford which owns the 102flowers data. 
-
-1). Remove the downloaded files
-
-rm -rf ~/tensorflow_datasets/oxford_flowers102/
-rm -rf ~/tensorflow_datasets/downloads
-
-2). Install tfds-nightly
-
-$ pip --no-cache-dir install tfds-nightly
-or 
-$ pip3 --no-cache-dir install tfds-nightly
+Use the command "run_experiment(n, large_data_set=False, generator=False)" to initiate the functional call.
+It show pictures, epochs/iterations and final score results but does not save the intermediate big data.
 """
 
 from __future__ import absolute_import
