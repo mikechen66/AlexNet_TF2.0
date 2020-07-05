7d8d2f6ce8e10f0876075c4fc2afcdfc0ac2516b
mikechen66 <875661606@qq.com>
2020-07-06 00:58:33 +0800

    Update alexnet_batchnorm.py
    It is the object oriented AlexNet with the BatchNormalization. It can show the model summary. It will be prepared for the Oxford_Flowers102 dataset from TensorFlows datasets API.

--- alexnet_batchnorm.py
+++ alexnet_batchnorm.py
@@ -8,9 +8,13 @@ AlexNet implementation in TensorFlow, with weights Details:
 #http://www.cs.toronto.edu/~guerzhoy/tf_alexnet/
 Stanford c231n 
 https://cs231n.github.io/convolutional-networks/#conv
+
+Editor: Mike Chen
+
 The script is the realization of command oriented style of AlexNet. It is similar to the original style 
 of AlexNet realization by Michael Guerzhoy on 2017. In consideration of the actual inputs with 1000 
-classes, we set the same class numbers. The model is distinguished with Batch Normalization. 
+classes, we set the same class numbers. 
+
 According to the formula of Stanford cs231, W_output=(W-F+2P)S+1. W,F,P,S are input width, filter width, 
 padding size and stride respectively. It is the apparent result of H_output=W_output since we requires 
 square size of filters. 
@@ -18,102 +22,82 @@ square size of filters.
 
 
 import tensorflow as tf
-from tensorflow import keras
+import keras
 from keras.models import Sequential
-from keras.layers import Dense, Activation, Dropout, Flatten, Conv2D, MaxPooling2D
+from keras.layers import Dense, Activation, Dropout, Flatten, Conv2D, MaxPooling2D,  BatchNormalization
+from keras import utils
+import functools
 
 
-# Set the class number as 1000
-num_classes = 1000
-# Set up the sequential model
-model = keras.Sequential()
+# Set the class number as 102
+num_classes = 102
 
+# Set up the top 5 error rate metric.
+top_5_acc = functools.partial(keras.metrics.top_k_categorical_accuracy, k=5)
+top_5_acc.__name__ = 'top_5_acc'
+
+# Instantiate an empty model
+model = Sequential()
 
 # 1st Convolutional Layer: (227-11+2*0)/4 + 1 = 55
-model.add(keras.layers.Conv2D(input_shape=(227, 227, 3),
-                              kernel_size=(11, 11),
-                              strides=(4, 4),
-                              padding="valid",
-                              filters=96,
-                              activation=tf.nn.relu))
-model.add(keras.layers.MaxPooling2D(pool_size=(3, 3),
-                                    strides=(2, 2),
-                                    padding="valid"))
-model.add(keras.layers.BatchNormalization())
-
+model.add(Conv2D(input_shape=(227,227,3), filters=96, kernel_size=(11,11), 
+                 strides=4, padding='valid', activation='relu'))
+# Max Pooling: (55-3+2*0)/2 + 1 = 27
+model.add(MaxPooling2D(pool_size=(3,3), strides=(2,2), padding='valid'))
+model.add(BatchNormalization())
 
 # 2nd Convolutional Layer: (27-5+2*2)/1 + 1 = 27
-model.add(keras.layers.Conv2D(kernel_size=(5, 5),
-                              strides=(1, 1),
-                              padding="same",
-                              filters=256,
-                              activation=tf.nn.relu))
-model.add(keras.layers.MaxPooling2D(pool_size=(3, 3),
-                                    strides=(2, 2),
-                                    padding="valid"))
-model.add(keras.layers.BatchNormalization())
-
+model.add(Conv2D(filters=256, kernel_size=(5,5), strides=1, 
+                 padding='same', activation='relu'))
+# Max Pooling: (27-3+2*0)/2 +1 = 13
+model.add(MaxPooling2D(pool_size=(3,3), strides=(2,2), padding='valid'))
+model.add(BatchNormalization())
 
 # 3rd Convolutional Layer: (13-3+ 2*1)/1 + 1 = 13
-model.add(keras.layers.Conv2D(kernel_size=(3,3),
-                              strides=(1,1),
-                              padding="same",
-                              filters=384,
-                              activation=tf.nn.relu))
-model.add(keras.layers.BatchNormalization())
-
+model.add(Conv2D(filters=384, kernel_size=(3,3), strides=1, 
+                 padding='same', activation='relu'))
+model.add(BatchNormalization())
 
 # 4th Convolutional Layer: (13-3+2*1) + 1 = 13
-model.add(keras.layers.Conv2D(kernel_size=(3,3),
-                              strides=(1,1),
-                              padding="same",
-                              filters=384,
-                              activation=tf.nn.relu))
-model.add(keras.layers.BatchNormalization())
-
+model.add(Conv2D(filters=384, kernel_size=(3,3), strides=1, 
+                 padding='same', activation='relu'))
+model.add(BatchNormalization())
 
 # 5th Convolutional Layer: (13-3+2*1) + 1 = 13
-model.add(keras.layers.Conv2D(kernel_size=(3,3),
-                              strides=(1,1),
-                              padding="same",
-                              filters=256,
-                              activation=tf.nn.relu))
-model.add(keras.layers.MaxPooling2D(pool_size=(3,3),
-                                    strides=(2,2),
-                                    padding="valid"))
-model.add(keras.layers.BatchNormalization())
-
+model.add(Conv2D(filters=256, kernel_size=(3,3), strides=1, 
+                 padding='same', activation='relu'))
+# Max Pooling: (13-3+2*0)/2 + 1 =  6
+model.add(MaxPooling2D(pool_size=(3,3), strides=(2,2), padding='valid'))
+model.add(BatchNormalization())
 
 # Flatten 256 x 6 x 6 = 9216 as one dimention vector and pass it to the 6th Fully
 # Connected layer
-model.add(keras.layers.Flatten())
+model.add(Flatten())
 
+# 6th Fully Connected Layer
+model.add(Dense(4096, activation='relu'))
+# Add Dropout
+model.add(Dropout(0.5))
+model.add(BatchNormalization())
 
-# 6th layer: fully connected layer with 4096 neurons with 50% dropout and batch normalization.
-model.add(keras.layers.Dense(units=4096,
-                             activation=tf.nn.relu))
-model.add(keras.layers.Dropout(rate=0.5))
-model.add(keras.layers.BatchNormalization())
+# 7th Fully Connected Layer
+model.add(Dense(4096, activation='relu'))
+# Add Dropout 
+model.add(Dropout(0.5))
+model.add(BatchNormalization())
 
+# No.8 FC Layer
+model.add(Dense(1000, activation='relu'))
+model.add(BatchNormalization())
 
-# 7th layer: fully connected layer with 4096 neurons with 50% dropout and batch normalization.
-model.add(keras.layers.Dense(units=4096,
-                             activation=tf.nn.relu))
-model.add(keras.layers.Dropout(rate=0.5))
-model.add(keras.layers.BatchNormalization())
-
-
-# 8th Layer: fully connected layer with 1000 neurons with 50% dropout and batch normalization.
-model.add(keras.layers.Dense(units=1000,
-                             activation=tf.nn.relu))
+# Output layer: softmax function of 102 classes of the dataset. This integer should be changed to match
+# the number of classes in your dataset if you change from Oxford_Flowers.
 model.add(Dense(num_classes, activation='softmax'))
-model.add(keras.layers.BatchNormalization())
 
-
-# Compile the model using Adam optimizer and categorical_crossentropy loss function.
-model.compile(optimizer='adam',
+# Compile the model
+model.compile(optimizer=tf.keras.optimizers.Adam(0.001),
               loss='categorical_crossentropy',
-              metrics=['accuracy'])
+              metrics=['acc', top_5_acc])
 
-# Give the model structure summary 
+# Show the AlexNet Model 
 model.summary()
