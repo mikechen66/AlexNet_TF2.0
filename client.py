0f7a29e59d2dcbe2f9a8c896af790e21eda503de
mikechen66 <875661606@qq.com>
2020-08-05 00:26:50 +0800

    Add one-hot code to comply with Keras 2.4.3
    Add the one-hot lines of code into the preprocessing od train images  in order to comply with TensotFlow 2.2 and Keras 2.4.3
    
        train_images = np.array(train_images)
        train_labels = np.array(train_labels)
        train_labels = utils.to_categorical(train_labels)

--- client.py
+++ client.py
@@ -195,14 +195,16 @@ def preprocess_data(data_train, data_test, data_val):
     # Since the images in Oxford_Flowers are not preprocessed, we need to resize them all so that the network
     # takes inputs that are all the same size. We will also transform the data to help the network generalize.
     for example in data_train.take(-1):
-        # Get images and labels from the Dataset object
+        # Get images and labels from the Dataset object, resize images and add to dataset
         image, label = example['image'], example['label']
-
-        # Resize images and add to dataset
         image = tf.image.convert_image_dtype(image, tf.float32)
         image = tf.image.resize(image, [227, 227])
         train_images.append(image.numpy())
         train_labels.append(label.numpy())
+
+    train_images = np.array(train_images)
+    train_labels = np.array(train_labels)
+    train_labels = utils.to_categorical(train_labels)
 
     
     # 2.Preprocess test images. 
@@ -223,7 +225,7 @@ def preprocess_data(data_train, data_test, data_val):
     test_labels = utils.to_categorical(test_labels)
 
     # 3.Preprocess val images. 
-    # Do the same as above but with the test and validation datasets.
+    
     val_images = []
     val_labels = []
 
